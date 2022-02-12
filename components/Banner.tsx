7246025d6dd7ba3751b450be05e64b07b8deaf6e
Thomasyong11 <akumyongt27@gmail.com>
2022-02-12 19:50:00 +0000

    ui changes

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,7 +4,7 @@ const Banner = () => {
     return <div className='flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0'>
         <div className='px-10 space-y-5'>
             <h1 className='text-6xl max-w-xl font-serif'>
-                <span className='underline decoration-black decoration-4'>Medium</span> is a place to write, read and connect
+                <span className='underline decoration-black decoration-4'>Yong's Medium</span> is a place where i write, read and connect
             </h1>
             <h2>
                 It's easy and free to post your thinking on any topic and connect
